db944c7ad266998f3b2be5cf88e269e7e5d8d933
loordbarringtn <quantas@gmail.com>
2024-06-22 01:51:13 +0300

    сделан рефакторинг. создан новый метод makeMoney() - который теперь приносит нам деньги!

--- src/main/java/api/tests/MakeTap.java
+++ src/main/java/api/tests/MakeTap.java
@@ -3,21 +3,50 @@ package api.tests;
 import api.Requests.PostRequest;
 import api.utils.ConfigLoader;
 import api.utils.Utils;
-import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class MakeTap {
     Utils utils = new Utils();
     ConfigLoader configLoader = new ConfigLoader();
+    Integer earnPerTap = 11;
+    PostRequest postRequest = new PostRequest(configLoader.getToken());
+
 
     @Test
     public void makeTap() {
-        RestAssured.baseURI = "https://api.hamsterkombat.io";
-        String body = "{\"count\":20,\"availableTaps\":6000,\"timestamp\":" + utils.generateTimestamp() + "}";
-
-        PostRequest postRequest = new PostRequest(configLoader.getToken());
+        String body = "{\"count\": " + earnPerTap + ",\"availableTaps\":6000,\"timestamp\":" + utils.generateTimestamp() + "}";
         Response response = postRequest.makePostRequest("tap", body).then().extract().response();
-        System.out.println("Response body: " + response.prettyPrint());
+    }
+
+    @Test
+    public void makeMoney() throws InterruptedException {
+        int availableTaps = 6000;
+        double balanceCoins = 0;
+
+        String body = "{\"count\": " + earnPerTap + ", \"availableTaps\": " + availableTaps + ", \"timestamp\": " + utils.generateTimestamp() + "}";
+        Response response = postRequest.makePostRequest("tap", body).then().extract().response();
+        JsonPath jsonPath = response.jsonPath();
+
+        availableTaps = jsonPath.getInt("clickerUser.availableTaps");
+        balanceCoins = jsonPath.getDouble("clickerUser.balanceCoins");
+
+        System.out.println("Available Taps: " + availableTaps);
+        System.out.println("Balance Coins: " + balanceCoins);
+
+        while (availableTaps > 0) {
+            body = "{\"count\": " + earnPerTap + ", \"availableTaps\": " + availableTaps + ", \"timestamp\": " + utils.generateTimestamp() + "}";
+            response = postRequest.makePostRequest("tap", body);
+            jsonPath = response.jsonPath();
+
+            availableTaps = jsonPath.getInt("clickerUser.availableTaps");
+            balanceCoins = jsonPath.getDouble("clickerUser.balanceCoins");
+
+            System.out.println("Available Taps: " + availableTaps);
+            System.out.println("Balance Coins: " + balanceCoins);
+
+            Thread.sleep(550);
+        }
     }
 }
